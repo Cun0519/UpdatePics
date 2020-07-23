d74b675aad0844cb2029bf54ef16581431f0cc12
cunxie <544842679@qq.com>
2020-07-23 14:44:13 +0800

    添加updateVideoTime

--- src/main/java/MainProcess.java
+++ src/main/java/MainProcess.java
@@ -1,4 +1,6 @@
+import com.drew.imaging.FileTypeDetector;
 import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.quicktime.QuickTimeMetadataReader;
 import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.MetadataReader;
@@ -11,19 +13,34 @@ import java.util.Date;
 
 public class MainProcess {
 
+    /**
+     * path为照片路径
+     * movePath为移动文件夹路径
+     * videoPath为视频路径
+     */
     private static String path = "C:\\Users\\CunXie\\Pictures\\iCloud";
     private static String movePath = "C:\\Users\\CunXie\\Pictures\\move\\";
     private static String videoPath = "C:\\Users\\CunXie\\Pictures\\MOV";
 
+    /**
+     * 在main()函数中根据需求去调用不同方法
+     * @param args
+     */
     public static void main(String[] args) {
-        File directory = new File(path);
+        File directory = new File(videoPath);
         int count = 0;
         int moveCount = 0;
         for (File pic : directory.listFiles()) {
-
+            int flag = updateVideoTime(pic);
+            switch (flag) {
+                case 1:
+                    count++;
+                case 0:
+                    moveCount++;
+            }
         }
         System.out.println(count);
-        System.out.println(moveCount);
+        //System.out.println(moveCount);
     }
 
     /**
@@ -39,10 +56,50 @@ public class MainProcess {
                 for (Tag tag : directory.getTags()) {
                     if (tag.getTagType() == 0x9003) {
                         flag = 1;
+
                         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
                         Date originalDate = simpleDateFormat.parse(tag.getDescription());
                         long time = originalDate.getTime();
                         pic.setLastModified(time);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    /**
+     * 将视频的修改时间更新为视频的拍摄时间
+     * @param video
+     * @return 视频如果包含拍摄时间则返回1，否则返回0
+     */
+    private static int updateVideoTime(File video) {
+        int flag = 0;
+        try {
+            Metadata metadata = QuickTimeMetadataReader.readMetadata(video);
+            for (Directory directory : metadata.getDirectories()) {
+                for (Tag tag : directory.getTags()) {
+                    if (tag.getTagType() == 256) {
+                        flag = 1;
+
+                        //日期转换
+                        String stringTemp = tag.getDescription();
+                        String[] stringArrayTemp = stringTemp.split(" ");
+                        stringArrayTemp[1] = stringArrayTemp[1].substring(0, stringArrayTemp[1].length() - 1);
+                        StringBuffer stringBufferTemp = new StringBuffer();
+                        stringBufferTemp.append(stringArrayTemp[5] + ":");
+                        stringBufferTemp.append(stringArrayTemp[1] + ":");
+                        stringBufferTemp.append(stringArrayTemp[2] + " ");
+                        stringBufferTemp.append(stringArrayTemp[3]);
+                        //System.out.println(tag.getDescription());
+                        //System.out.println(stringBufferTemp);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+                        Date originalDate = simpleDateFormat.parse(stringBufferTemp.toString());
+                        long time = originalDate.getTime();
+                        video.setLastModified(time);
                     }
                 }
             }
